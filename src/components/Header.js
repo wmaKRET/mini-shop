3c209853e84aa9a3a03382e8133cf5efd7e273f6
wmaKRET <wkret91.2@gmail.com>
2022-07-08 11:39:15 +0200

    installed remixicon-react, updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from "react"
 import {Link} from "react-router-dom"
+import EmptyCart from "remixicon-react/ShoppingCartLineIcon"
+import fullCart from "remixicon-react/ShoppingCartFillIcon"
 
 function Header(){
     return (
         <header>
             <Link className="header-link" to="/">Mini Shop</Link>
-            <Link className="header-link" to="/cart">Cart</Link>
+            <Link className="header-link" to="/cart">
+                <EmptyCart />
+            </Link>
         </header>
     )
 }
